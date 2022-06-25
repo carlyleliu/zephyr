9371214e3750632f080970e0c16d94d1bd2f7bb5
carlyleliu <yyliushuai@gmail.com>
2022-06-25 17:44:37 +0800

    fixed sram and flash for stm32f429vet6

--- project/robot_chassis/src/main.c
+++ project/robot_chassis/src/main.c
@@ -13,13 +13,40 @@
 #include <zephyr/sys/printk.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/pwm.h>
+#include <zephyr/drivers/gpio.h>
 
+/* 1000 msec = 1 sec */
+#define SLEEP_TIME_MS   1000
+
+/* The devicetree node identifier for the "led0" alias. */
+#define LED0_NODE DT_ALIAS(led0)
+
+/*
+ * A build error on this line means your board is unsupported.
+ * See the sample documentation for information on how to fix this.
+ */
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 void main(void)
 {
-	printk("hello world\n");
-	while (1)
-	{
-		;
+	int ret;
+
+	printk("system start...\n");
+
+	if (!device_is_ready(led.port)) {
+		return;
+	}
+
+	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0) {
+		return;
+	}
+
+	while (1) {
+		ret = gpio_pin_toggle_dt(&led);
+		if (ret < 0) {
+			return;
+		}
+		k_msleep(SLEEP_TIME_MS);
 	}
 }
